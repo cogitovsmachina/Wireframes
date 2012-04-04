0e754fb9e16e5a4053f64ad451720fb9f08bfec1
Enrique Diaz <nrikediaz@androidtitlan.org>
2012-04-04 06:31:04 -0500

    Testing dropdown list in WireframeActivity

--- gen/com/senselabs/wireframe/R.java
+++ gen/com/senselabs/wireframe/R.java
@@ -8,6 +8,9 @@
 package com.senselabs.wireframe;
 
 public final class R {
+    public static final class array {
+        public static final int sections=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -22,14 +25,14 @@ public final class R {
         public static final int window_background_home=0x7f020008;
     }
     public static final class id {
-        public static final int action_copy=0x7f070003;
-        public static final int action_delete=0x7f070004;
-        public static final int action_edit_or_view=0x7f070001;
-        public static final int action_new=0x7f070002;
-        public static final int action_new_screen=0x7f070005;
-        public static final int action_preview=0x7f070006;
-        public static final int action_send_via_email=0x7f070007;
-        public static final int custom_layout=0x7f070000;
+        public static final int action_copy=0x7f080003;
+        public static final int action_delete=0x7f080004;
+        public static final int action_edit_or_view=0x7f080001;
+        public static final int action_new=0x7f080002;
+        public static final int action_new_screen=0x7f080005;
+        public static final int action_new_view=0x7f080007;
+        public static final int action_send_via_email=0x7f080006;
+        public static final int custom_layout=0x7f080000;
     }
     public static final class layout {
         public static final int custom_switch_layout=0x7f030000;
@@ -37,27 +40,27 @@ public final class R {
         public static final int wireframe_layout=0x7f030002;
     }
     public static final class menu {
-        public static final int home_menu=0x7f060000;
-        public static final int wireframe_menu=0x7f060001;
+        public static final int home_menu=0x7f070000;
+        public static final int wireframe_menu=0x7f070001;
     }
     public static final class string {
-        public static final int action_copy=0x7f040005;
-        public static final int action_delete=0x7f040004;
-        public static final int action_edit=0x7f040009;
-        public static final int action_layouts=0x7f04000c;
-        public static final int action_navigate=0x7f04000a;
-        public static final int action_new=0x7f040003;
-        public static final int action_new_screen=0x7f040008;
-        public static final int action_preview=0x7f040006;
-        public static final int action_save=0x7f040002;
-        public static final int action_send_via_email=0x7f040007;
-        public static final int action_views=0x7f04000b;
-        public static final int app_name=0x7f040000;
-        public static final int wireframe_activity_label=0x7f040001;
+        public static final int action_copy=0x7f050005;
+        public static final int action_delete=0x7f050004;
+        public static final int action_edit=0x7f050009;
+        public static final int action_layouts=0x7f05000c;
+        public static final int action_navigate=0x7f05000a;
+        public static final int action_new=0x7f050003;
+        public static final int action_new_screen=0x7f050008;
+        public static final int action_preview=0x7f050006;
+        public static final int action_save=0x7f050002;
+        public static final int action_send_via_email=0x7f050007;
+        public static final int action_views=0x7f05000b;
+        public static final int app_name=0x7f050000;
+        public static final int wireframe_activity_label=0x7f050001;
     }
     public static final class style {
-        public static final int ActionBar=0x7f050002;
-        public static final int ActionBarText=0x7f050003;
+        public static final int ActionBar=0x7f060002;
+        public static final int ActionBarText=0x7f060003;
         /**  Start with the Holographic Light theme 
  custom attributes 
 
@@ -65,7 +68,7 @@ public final class R {
         <item name="trackAbstractMaxLines">@integer/track_abstract_max_lines</item>
         
          */
-        public static final int Theme_IOSched=0x7f050000;
-        public static final int Theme_IOSched_Home=0x7f050001;
+        public static final int Theme_IOSched=0x7f060000;
+        public static final int Theme_IOSched_Home=0x7f060001;
     }
 }
--- src/com/senselabs/wireframe/WireframeActivity.java
+++ src/com/senselabs/wireframe/WireframeActivity.java
@@ -1,11 +1,15 @@
 package com.senselabs.wireframe;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ActionBar.OnNavigationListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class WireframeActivity extends Activity {
 
@@ -14,6 +18,24 @@ public class WireframeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.wireframe_layout);
 		// Opens a Canvas to do Drag & Drop
+
+		// set up list nav
+
+		final ActionBar actionBar = getActionBar();
+		actionBar.setListNavigationCallbacks(ArrayAdapter.createFromResource(
+				this, R.array.sections,
+				android.R.layout.simple_spinner_dropdown_item),
+				new OnNavigationListener() {
+					public boolean onNavigationItemSelected(int itemPosition,
+							long itemId) {
+						// FIXME add proper implementation
+						Toast.makeText(getApplicationContext(), "Works",
+								Toast.LENGTH_SHORT).show();
+
+						return false;
+					}
+
+				});
 
 	}
 
@@ -38,11 +60,21 @@ public class WireframeActivity extends Activity {
 			// Do something related to our delete objects in project gridview
 			return true;
 		case R.id.action_edit_or_view:
-//			item.setActionView(R.layout.custom);
-			
+			// item.setActionView(R.layout.custom);
+		case R.id.action_new_view:
+			showDropDownNav();
+
 		}
 
 		return false;
+	}
+
+	private void showDropDownNav() {
+		ActionBar ab = getActionBar();
+		if (ab.getNavigationMode() != ActionBar.NAVIGATION_MODE_LIST) {
+			ab.setDisplayShowTitleEnabled(false);
+			ab.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		}
 	}
 
 }
