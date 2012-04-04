bced7e44de1457da3f2e7e3f60b80909b6b798f9
Enrique Diaz <nrikediaz@androidtitlan.org>
2012-04-04 05:04:53 -0500

    Initial commit

---
+++ src/com/senselabs/wireframe/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.senselabs.wireframe;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        //Look for Projects in SD, if there are projects then show in a gridview
+        
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.home_menu, menu);
+	    return true;		
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                startActivity(new Intent(getApplicationContext(), WireframeActivity.class));
+                return true;
+            case R.id.action_copy:
+            	//Do something related to our copy objects in project gridview
+            	return true;
+        
+            case R.id.action_delete:
+            	//Do something related to our delet objects in project gridview
+            	return true;
+        }
+        
+        return false;
+    }
+}
---
+++ src/com/senselabs/wireframe/WireframeActivity.java
@@ -0,0 +1,48 @@
+package com.senselabs.wireframe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class WireframeActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.wireframe_layout);
+		// Opens a Canvas to do Drag & Drop
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.wireframe_menu, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_new:
+			startActivity(new Intent(getApplicationContext(),
+					WireframeActivity.class));
+			return true;
+		case R.id.action_copy:
+			// Do something related to our copy objects in project gridview
+			return true;
+
+		case R.id.action_delete:
+			// Do something related to our delete objects in project gridview
+			return true;
+		case R.id.action_edit_or_view:
+//			item.setActionView(R.layout.custom);
+			
+		}
+
+		return false;
+	}
+
+}
